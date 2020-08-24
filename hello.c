e3ba72ff44c9e84d6ab45019ef0f25e75dfdb713
Jacob Harvey <66391376+jacoblharvey@users.noreply.github.com>
2020-08-24 14:49:59 -0500

    Update hello.c
    Add lines to print name and major for Jacob Harvey

--- hello.c
+++ hello.c
@@ -1,16 +1,19 @@
 /**
  * Author Ryker Henderson
  * Date:  8/18/20
- * 
+ *
  * A simple hello world program
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
+
      printf("Ryker Henderson!\n");
-     printf("Computer Engineering");
+     printf("Computer Engineering"\n);
+     printf("Jacob Harvey"\n);
+     printf("Electrical Engineering"\n);
+
      return 0;
-     
+
  }
