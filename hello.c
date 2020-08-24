f6d7b42248461806e3fb95d9d4a900e1d8db207e
ryker2333 <69864512+ryker2333@users.noreply.github.com>
2020-08-24 10:56:09 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author Ryker Henderson
+ * Date:  8/18/20
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Hello world!\n");
+     
+     return 0;
+     
+ }
